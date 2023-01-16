890b2ef371bc5faf808d6fa86d972809244a3bd3
Rashdan Natiq <rashdan@natiq.se>
2023-01-16 11:33:16 +0500

    Initial Commit

---
+++ FancyLoginSignup/FancyLoginSignupApp.swift
@@ -0,0 +1,17 @@
+//
+//  FancyLoginSignupApp.swift
+//  FancyLoginSignup
+//
+//  Created by Rashdan on 2023-01-16.
+//
+
+import SwiftUI
+
+@main
+struct FancyLoginSignupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
